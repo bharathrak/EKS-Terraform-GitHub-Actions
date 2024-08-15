10ac6bc425ec20cfad9a3a590424dd9ef36f3711
bharathrak <162553277+bharathrak@users.noreply.github.com>
2024-08-15 08:22:45 +0530

    Update backend.tf
    changed bucket name

--- eks/backend.tf
+++ eks/backend.tf
@@ -7,7 +7,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "my-eks-tera-buckete-00123-github"
+    bucket         = "my-eks-tera-buckete-00123-git-hub"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
     dynamodb_table = "Lock-Files"
